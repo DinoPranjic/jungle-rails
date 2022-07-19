652d02984e785f9ea686facc24facdfbd4b177c8
DinoPranjic <dinopranjic92@gmail.com>
2022-07-19 03:25:19 +0000

    add add-to-cart cypress test

---
+++ cypress/integration/add_to_cart.spec.js
@@ -0,0 +1,12 @@
+describe('testing add to cart functionality', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
+  it("cart count increases by one", () => {
+    cy.get('#2').click({force: true})
+    cy.get('[data-cy="cart"]').contains("My Cart (1)")
+    // better way is to assign data-attribute
+  })
+
+})
